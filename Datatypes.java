db38dafbce8b4c71769c3de0a630633236cdb73f
Ananya6622 <ananyaetti6622@gmail.com>
2023-06-05 22:53:22 +0530

    Added

---
+++ Datatypes.java
@@ -0,0 +1,25 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        boolean flag = true;
+        System.out.println("boolean " + flag);
+
+        byte bytevalue = 127;
+        System.out.println("the byte value " + bytevalue);
+
+        short shortvalue = 23456;
+        System.out.println("The short value " + shortvalue);
+
+        long longvalue = 98765432l;
+        System.out.println("Long value is" + longvalue);
+
+        float floatvalue = 5.03f;
+        System.out.println("the float value " + floatvalue);
+
+        double doublevalue = 23456.05;
+        System.out.println("The double value " + doublevalue);
+
+        char charvalue ='A';
+        System.out.println("The char value "+ charvalue);
+
+    }
+}
---
+++ StringEqual.java
@@ -0,0 +1,14 @@
+package java1;
+
+public class StringEqual {
+    public static void main(String[] args) {
+        String String1 = "Ananya";
+        String String2 = "Honey";
+
+        if (String1.equals(String2)){
+            System.out.println("string is equal");
+        }else {
+            System.out.println("string is not equal");
+        }
+    }
+}
